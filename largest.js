325eb4807cadfbc4f2a1bfd617d65526373e88d2
imbidhya <65336886+imbidhya@users.noreply.github.com>
2022-03-04 20:06:40 +0545

    Add files via upload

---
+++ largest.js
@@ -0,0 +1,14 @@
+let a=5;
+let b=7;
+let c=9;
+if(a>b && a>c)
+{
+    console.log("a is largest number")
+
+}
+else if(b>a && b>c){
+    console.log("b is largest");
+}
+else{
+    console.log("c is largest")
+}
